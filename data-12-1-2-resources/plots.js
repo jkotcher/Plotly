c4e28a0ecb98714113565fd49952fcd974d99299
jkotcher <jacob.kotcher@gmail.com>
2022-08-16 10:40:08 -0400

    Bar chart

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -1,4 +1,15 @@
-Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+var trace = {
+    x: ["burrito", "pizza", "chicken"],
+    y: [10, 18, 5],
+    type: "bar"
+};
+
+var layout = {
+    title: "Luncheon Survey"
+};
+
+Plotly.newPlot("plotArea", [trace], layout);
+
 
 
 Plotly.newPlot("plotArea2", [{x: [5, 10, 15], y: [10, 20, 30]}]);
