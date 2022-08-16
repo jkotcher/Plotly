5a3dddce0bf74b2f24924b650b3bed73b40f814a
jkotcher <jacob.kotcher@gmail.com>
2022-08-16 11:09:31 -0400

    scatter plot practice

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -44,17 +44,39 @@ var trace = {
   };
   Plotly.newPlot("plotArea3", data, layout);
 
+ 
+ //Scatter plot practice
   var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
+    x: [1, 2, 3, 4, 5],
+    y: [1, 6, 3, 6, 1],
     mode: 'markers',
-    type: 'scatter'
+    type: 'scatter',
+    name: 'Team A',
+    text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+    marker: { size: 12 }
   };
-
+  
+  var trace2 = {
+    x: [1.5, 2.5, 3.5, 4.5, 5.5],
+    y: [4, 1, 7, 1, 4],
+    mode: 'markers',
+    type: 'scatter',
+    name: 'Team B',
+    text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
+    marker: { size: 12 }
+  };
+  
+  var data = [ trace1, trace2 ];
+  
   var layout = {
-    title: "Practice scatter plot",
-    xaxis: {title: "height"},
-    yaxis: {title: "width"}
+    xaxis: {
+      range: [ 0.75, 5.25 ]
+    },
+    yaxis: {
+      range: [0, 8]
+    },
+    title:'Data Labels Hover'
   };
+  
+  Plotly.newPlot('plotArea4', data, layout);
 
-  Plotly.newPlot("plotArea4", trace1, layout)
