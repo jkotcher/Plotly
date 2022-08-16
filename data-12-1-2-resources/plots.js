26ccd80f6aacd59268f753b76db04dac570927c5
jkotcher <jacob.kotcher@gmail.com>
2022-08-16 10:52:08 -0400

    Adding pie chart

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -5,11 +5,41 @@ var trace = {
 };
 
 var layout = {
-    title: "Luncheon Survey"
+    title: "Luncheon Survey",
+
+    xaxis: {title: "Food Option"},
+
+    yaxis: {title: "Number of Respondents"}
 };
 
 Plotly.newPlot("plotArea", [trace], layout);
 
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: "bar"
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Bar' Chart",
+    xaxis: {title: "Drinks"},
+    yaxis: {title: "% of Drinks Ordered"}
+   };
+   Plotly.newPlot("plotArea", data, layout);
+
 
 
 Plotly.newPlot("plotArea2", [{x: [5, 10, 15], y: [10, 20, 30]}]);
+
+
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+       "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+  };
+  var data = [trace];
+  var layout = {
+    title: "'Pie' Chart",
+  };
+  Plotly.newPlot("plotArea3", data, layout);
