f7903f85a391f75aca3dcf42d299404a62a778d5
jkotcher <jacob.kotcher@gmail.com>
2022-08-18 11:05:18 -0400

    Adding forEach loop over BB data

--- JS/plots.js
+++ JS/plots.js
@@ -1,3 +1,10 @@
 d3.json("JS/data/samples.json").then(function(data){
     console.log(data);
 });
+
+
+d3.json("JS/data/samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
