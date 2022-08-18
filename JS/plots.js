a2de674d10a5eef1fb840737d7ef3771c29a7b8a
jkotcher <jacob.kotcher@gmail.com>
2022-08-18 15:45:05 -0400

    Creating functions and deploying sample project

--- JS/plots.js
+++ JS/plots.js
@@ -30,3 +30,22 @@ function init() {
   };
   
   init();
+
+
+function optionChanged(newSample) {
+    console.log(newSample);
+};
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+  }
+
+
